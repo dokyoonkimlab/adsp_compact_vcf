dcb30e0d4397a9b74b99c2d94740a4c1f76247e7
wanpinglee <wanping.lee@gmail.com>
2021-02-01 11:29:36 -0500

    Implement filter feature in compact_filter_vcf.cpp

--- compact_filter_vcf.cpp
+++ compact_filter_vcf.cpp
@@ -7,9 +7,9 @@
 struct Variant {
 	// the CHROM, ID, REF, ALT, QUAL, FILTER, INFO, and FORMAT columns in VCF
 	std::string chr, id, ref, alt, qual, filter, info, format;
-	int pos, ac, an; // position, AC, and AN.
-	int qc_dp, qc_gq, qc_both; // DP < 10 and/or GQ <20.
-	int ac_qc, an_qc; // AC and AN after QC.
+	int pos, info_ac, info_an, info_dp; // position, AC, AN, and DP in INFO.
+	int qc_fail_dp, qc_fail_gq, qc_fail_both; // DP < 10 and/or GQ <20.
+	int ac_qc, an_qc, dp_qc; // AC and AN after QC.
 	std::vector<std::string> gt;
 	std::vector<std::string> dp;
 	std::vector<std::string> gq;
@@ -24,9 +24,9 @@ struct Variant {
 		filter.clear();
 		info.clear();
 		format.clear();
-		pos=0, ac=0, an=0;
-		qc_dp=0, qc_gq=0, qc_both=0;
-		ac_qc=0, an_qc=0;
+		pos=0, info_ac=0, info_an=0, info_dp=0;
+		qc_fail_dp=0, qc_fail_gq=0, qc_fail_both=0;
+		ac_qc=0, an_qc=0, dp_qc=0;
 		gt.clear();
 		dp.clear();
 		gq.clear();
@@ -45,6 +45,28 @@ inline void GetBasicInfo (Variant & var, std::stringstream & ss) {
 	ss >> var.filter;
 	ss >> var.info;
 	ss >> var.format;
+
+	std::size_t found1 = var.info.find("AC=");
+	std::size_t found2 = var.info.find(";", found1 + 1);
+	var.info_ac = std::atoi(var.info.substr(found1 + 3, found2 - found1 - 3).c_str());
+
+	found1 = var.info.find("AN=");
+	found2 = var.info.find(";", found1 + 1);
+	var.info_an = std::atoi(var.info.substr(found1 + 3, found2 - found1 - 3).c_str());
+
+	found1 = var.info.find("DP=");
+	found2 = var.info.find(";", found1 + 1);
+	var.info_dp = std::atoi(var.info.substr(found1 + 3, found2 - found1 - 3).c_str());
+}
+
+//Add "ori" as a prefix to AN, AC, AF, and DP in INFO field.
+inline void AddPrefix (const std::string & prefix, const std::string & target, std::string & info) {
+	const std::size_t found = info.find(target);
+	if (found != std::string::npos)
+		info.insert(found, prefix);
+	else
+		std::cerr << "Warning: Cannot find " << target << " in " << info << std::endl;
+		
 }
 
 // Keep falgs and ABHet in INFO and discard others
@@ -86,11 +108,13 @@ bool ExtractInfo (Variant & var, std::stringstream & ss) {
 	std::string token;
 	//bool all_missing = true;
 	bool first = true;
+	const std::string missingGt = "./.";
 	while (std::getline(ss, token, '\t')) { // Divid by tab
 		if (first) { // The first one is the empty one.
 			first = false;
 			continue;
 		}
+		//std::cerr << token << std::endl;
 		std::size_t pos = token.find(':'); // GT is the first info in ADSP VCF
 		const std::string gt = token.substr(0, pos); // Extract GT
 		var.gt.push_back(gt); // Store GT
@@ -106,22 +130,23 @@ bool ExtractInfo (Variant & var, std::stringstream & ss) {
 		if (gt[0] != '.' && gt[0] != '0') ac++;
 		if (gt[2] != '.' && gt[2] != '0') ac++;
 
-		var.an += an;
-		var.ac += ac;
 		var.an_qc += an;
 		var.ac_qc += ac;
 		
 		// For AD
-		pos = token.find(':'), pos + 1; // AD
+		pos = token.find(':', pos + 1); // AD
 
 		// For DP
 		std::size_t pos1 = token.find((':'), pos + 1); // DP
 		//var.dp.push_back(token.substr(pos + 1, pos1 - pos - 1));
-		const bool qc_dp_fail = (std::atoi(token.substr(pos + 1, pos1 - pos - 1).c_str()) < 10) ? true : false;
+		const int dp = std::atoi(token.substr(pos + 1, pos1 - pos - 1).c_str());
+		//std::cerr << std::atoi(token.substr(pos + 1, pos1 - pos - 1).c_str()) << std::endl;
+		const bool qc_dp_fail = (dp < 10) ? true : false;
 		if (qc_dp_fail) {
-			var.qc_dp++;
+			var.qc_fail_dp++;
 			var.an_qc -= an;
 			var.ac_qc -= ac;
+			*(var.gt.rbegin()) = missingGt;
 		}
 
 		// For GQ
@@ -129,13 +154,17 @@ bool ExtractInfo (Variant & var, std::stringstream & ss) {
 		pos1 = token.find((':'), pos + 1); // GQ
 		//var.gq.push_back(token.substr(pos + 1, pos1 - pos - 1));
 		const bool qc_gq_fail = (std::atoi(token.substr(pos + 1, pos1 - pos - 1).c_str()) < 20) ? true : false;
-		if (qc_gq_fail) var.qc_gq++;
+		//std::cerr << std::atoi(token.substr(pos + 1, pos1 - pos - 1).c_str()) << std::endl;
+		//const bool qc_gq_fail = false;
+		if (qc_gq_fail) var.qc_fail_gq++;
 		if (qc_gq_fail && !qc_dp_fail) {
 			var.an_qc -= an;
 			var.ac_qc -= ac;
+			*(var.gt.rbegin()) = missingGt;
 		}
 
-		if (qc_dp_fail && qc_gq_fail) var.qc_both++;
+		if (!qc_dp_fail && !qc_gq_fail) var.dp_qc += dp;
+		if (qc_dp_fail && qc_gq_fail) var.qc_fail_both++;
 	}
 	//return all_missing;
 	return false;
@@ -143,6 +172,7 @@ bool ExtractInfo (Variant & var, std::stringstream & ss) {
 
 void PrintVariant(const Variant & var) {
 	//if (!var.pass) return;
+	const float af = (var.an_qc == 0) ? 0 : var.ac_qc/var.an_qc;
 	std::cout << var.chr << "\t"
 		<< var.pos << "\t"
 		<< var.id << "\t"
@@ -150,8 +180,8 @@ void PrintVariant(const Variant & var) {
 		<< var.alt << "\t"
 		<< var.qual << "\t"
 		<< var.filter << "\t"
-		<< var.info << "\t"
-		//<< "AC=" << var.ac << ";AF=" << var.ac/var.an << ";AN=" << var.an << ";" << var.info << "\t"
+		//<< var.info << "\t"
+		<< "AC=" << var.ac_qc << ";AF=" << af << ";AN=" << var.an_qc << ";DP=" << var.dp_qc << ";" << var.info << "\t"
 		<< var.format;
 	// Print GT of each sample
 	for (unsigned int i = 0; i < var.gt.size(); ++i) // The first one GT is empty, so we skip it.
@@ -161,29 +191,41 @@ void PrintVariant(const Variant & var) {
 
 int main (int argc, char** argv) {
 
-	// USAGE: zcat vcf.gz | <PROGRAM> | gzip > output.vcf.gz 2> info.txt
+	// USAGE: zcat vcf.gz | <PROGRAM> 2> info.txt | gzip > output.vcf.gz
 
 	std::ios_base::sync_with_stdio(false); // Fastern IO
 	std::cin.tie(NULL); // Fastern IO
 
 	std::string line; // buffer
-	std::cerr << "AC\tAN\tQC_AC\tQC_AN\tDP<10\tGQ<20\tDP_GQ" << std::endl;
+	std::cerr << "ORI_AC\tORI_AN\tORI_DP\tQC_AC\tQC_AN\tQC_DP\tDP<10\tGQ<20\tDP<10_AND_GQ<20" << std::endl;
 	while (std::getline(std::cin, line)) { // Catch a line from stdin
 		if (line[0] == '#') { // for header section, write out directly
 			std::cout << line << std::endl;
+			// Insert new tags in INFO field
+			if (line.find("ID=AC") != std::string::npos || line.find("ID=AN") != std::string::npos
+				|| line.find("ID=AF") != std::string::npos || line.find("ID=DP") != std::string::npos) {
+				const std::size_t found = line.find("ID=");
+				line.insert(found + 3, "ori_");
+				std::cout << line << std::endl;
+			}
 		} else {
 			std::stringstream ss(line); // Convert line to stringstream
 			Variant var;
 			var.clean();
 			GetBasicInfo(var, ss);
+			AddPrefix("ori_", "AC=", var.info);
+			AddPrefix("ori_", "AN=", var.info);
+			AddPrefix("ori_", "AF=", var.info);
+			AddPrefix("ori_", "DP=", var.info);
 			var.format = "GT"; // Only keep GT
 			//var.pass = KeepFlag(var.info); // rephase info; keep only VFLAGS and ABHet
-			ExtractInfo(var, ss);
+			ExtractInfo(var, ss); // Change low-qual genotypes to missing
 			PrintVariant(var); // Output vcf to stdout
-			std::cerr << var.ac << "\t" << var.an << "\t" << var.ac_qc << "\t" 
-			<< var.an_qc << "\t" << var.qc_dp << "\t" << var.qc_gq << "\t" << var.qc_both << std::endl;
+			std::cerr << var.info_ac << "\t" << var.info_an << "\t" << var.info_dp << "\t" << var.ac_qc << "\t" 
+			<< var.an_qc << "\t" << var.dp_qc << "\t" << var.qc_fail_dp << "\t" << var.qc_fail_gq << "\t" << var.qc_fail_both << std::endl;
 		}
 	}
 
+	std::cerr << "Program is done normally." << std::endl;
 	return 0;
 }
